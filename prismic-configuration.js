fe0399d897623884d4bb8d2110ac43e32978f3b3
dorian <lauren_d@outlook.fr>
2020-01-23 17:00:00 +0100

    nouveau site

---
+++ prismic-configuration.js
@@ -0,0 +1,8 @@
+module.exports = {
+  accessToken: 'MC5YaW5CdFJBQUFDRUE1WjZ4.77-977-9Zx4uVO-_vVnvv73vv73vv70JSFrvv71P77-977-9BO-_ve-_vVHvv73vv70r77-9Fu-_vQjvv70u77-9',
+
+  clientId: 'XimlnhAAACIA5R_N',
+  clientSecret: '7486602871e244cd1d14fe4194fae971',
+
+  endpoint: 'https://lkdigital.cdn.prismic.io/api/v2'
+}
